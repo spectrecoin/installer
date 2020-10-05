64aeea9371e1d99d8fda52891e3007b21df3d516
Helix <hlxeasy@gmail.com>
2020-10-05 20:45:23 +0200

    Prepared situation, where the "old" Spectrecoin repository is really no longer available, even if using the new Alias url

--- linux/updateSpectrecoin.sh
+++ linux/updateSpectrecoin.sh
@@ -17,6 +17,15 @@
 # ============================================================================
 
 versionToInstall=$1
+# The update script on the local machines don't know about the possible
+# existing releases on the new repository. So if something else than
+# 'latest' is given, pin it to the first Alias release
+#if [ "${versionToInstall}" = 'latest' ] ; then
+#    echo "Upgrading from Spectrecoin to latest Alias wallet release"
+#else
+#    versionToInstall="4.3.0"
+#    echo "Upgrading from Spectrecoin to Alias v${versionToInstall}"
+#fi
 
 # ----------------------------------------------------------------------------
 # Use ca-certificates if available
@@ -24,6 +33,6 @@ if [[ -e /etc/ssl/certs/ca-certificates.crt ]] ; then
     cacertParam='--cacert /etc/ssl/certs/ca-certificates.crt'
 fi
 
-echo "Upgrading from Spectrecoin to Alias:"
+echo "Upgrading from Spectrecoin to Alias (${versionToInstall}):"
 
 curl ${cacertParam} -L -s https://raw.githubusercontent.com/aliascash/installer/master/linux/migrateSpectrecoinToAlias.sh | sudo bash -s "${versionToInstall}"
