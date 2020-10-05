5dea58ad818811fb7a6e1db97ca00ee9480dd3e2
Helix <hlxeasy@gmail.com>
2020-10-05 20:10:26 +0200

    Fixed naming of migration script

--- linux/updateSpectrecoin.sh
+++ linux/updateSpectrecoin.sh
@@ -26,4 +26,4 @@ fi
 
 echo "Upgrading from Spectrecoin to Alias:"
 
-curl ${cacertParam} -L -s https://raw.githubusercontent.com/aliascash/installer/master/linux/updateAliaswallet.sh | sudo bash -s "${versionToInstall}"
+curl ${cacertParam} -L -s https://raw.githubusercontent.com/aliascash/installer/master/linux/migrateSpectrecoinToAlias.sh | sudo bash -s "${versionToInstall}"
